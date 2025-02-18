76449c108571ee05a6163587340a1b8c51f39d4d
lebaudantoine <lebaud.antoine131@gmail.com>
2025-02-18 23:55:48 +0100

    wip implement waiting room in the frontend

--- src/frontend/src/features/rooms/api/ApiRoom.ts
+++ src/frontend/src/features/rooms/api/ApiRoom.ts
@@ -1,14 +1,16 @@
+export type ApiLiveKit = {
+  url: string
+  room: string
+  token: string
+}
+
 export type ApiRoom = {
   id: string
   name: string
   slug: string
   is_administrable: boolean
   access_level: string
-  livekit?: {
-    url: string
-    room: string
-    token: string
-  }
+  livekit?: ApiLiveKit
   configuration?: {
     [key: string]: string | number | boolean
   }
