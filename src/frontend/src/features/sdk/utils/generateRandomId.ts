29462ca8663c516217224a620cacc77c1b203285
lebaudantoine <lebaud.antoine131@gmail.com>
2025-03-28 20:00:19 +0100

    wip create few utils

---
+++ src/frontend/src/features/sdk/utils/generateRandomId.ts
@@ -0,0 +1,17 @@
+/**
+ * Generates a random string of specified length
+ * @param {number} length - Length of the string to generate
+ * @returns {string} Random string
+ */
+export const generateRandomId = (length) => {
+  const characters =
+    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+  let result = ''
+
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length)
+    result += characters.charAt(randomIndex)
+  }
+
+  return result
+}
