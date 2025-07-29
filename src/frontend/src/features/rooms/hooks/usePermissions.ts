e3251bb9801b619e9fa267b762d6ac04697b2afe
lebaudantoine <lebaud.antoine131@gmail.com>
2025-07-29 18:32:59 +0200

    wip hook to watch permissions

---
+++ src/frontend/src/features/rooms/hooks/usePermissions.ts
@@ -0,0 +1,65 @@
+import { useEffect, useState } from 'react'
+
+type PermissionState = undefined | 'granted' | 'prompt' | 'denied'
+
+export const usePermissions = () => {
+  const [cameraPermission, setCameraPermission] =
+    useState<PermissionState>(undefined)
+  const [microphonePermission, setMicrophonePermission] =
+    useState<PermissionState>(undefined)
+
+  useEffect(() => {
+    const checkPermissions = async () => {
+      try {
+        const [cameraPermission, microphonePermission] = await Promise.all([
+          navigator.permissions.query({ name: 'camera' }),
+          navigator.permissions.query({ name: 'microphone' }),
+        ])
+        setCameraPermission(cameraPermission.state)
+        setMicrophonePermission(microphonePermission.state)
+
+        const handleCameraChange = (e: Event) => {
+          const target = e.target as PermissionStatus
+          setCameraPermission(target.state)
+        }
+
+        const handleMicrophoneChange = (e: Event) => {
+          const target = e.target as PermissionStatus
+          setMicrophonePermission(target.state)
+        }
+
+        if (!cameraPermission.onchange) {
+          cameraPermission.addEventListener('change', handleCameraChange)
+        }
+        if (!microphonePermission.onchange) {
+          microphonePermission.addEventListener(
+            'change',
+            handleMicrophoneChange
+          )
+        }
+
+        return () => {
+          cameraPermission.removeEventListener('change', handleCameraChange)
+          microphonePermission.removeEventListener(
+            'change',
+            handleMicrophoneChange
+          )
+        }
+      } catch (error) {
+        console.error('Error checking permissions:', error)
+      }
+    }
+    checkPermissions()
+  }, [])
+
+  return {
+    cameraPermission,
+    microphonePermission,
+    isCameraDenied: cameraPermission == 'denied',
+    isCameraPrompted: cameraPermission == 'prompt',
+    isCameraGranted: cameraPermission == 'granted',
+    isMicrophoneDenied: microphonePermission == 'denied',
+    isMicrophonePrompted: microphonePermission == 'prompt',
+    isMicrophoneGranted: microphonePermission == 'granted',
+  }
+}
