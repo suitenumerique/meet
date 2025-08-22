8a3a0d5759458763d94650263e5ac367889e31ba
lebaudantoine <lebaud.antoine131@gmail.com>
2025-08-22 12:03:04 +0200

    🔥(frontend) remove obsolete props from duplicated LiveKit controlbar
    Simplify duplicated controlbar component by removing props that became
    useless after solving prop drilling issues.

--- src/frontend/src/features/rooms/livekit/prefabs/ControlBar/ControlBar.tsx
+++ src/frontend/src/features/rooms/livekit/prefabs/ControlBar/ControlBar.tsx
@@ -6,45 +6,13 @@ import { DesktopControlBar } from './DesktopControlBar'
 import { SettingsDialogProvider } from '../../components/controls/SettingsDialogContext'
 import { useIsMobile } from '@/utils/useIsMobile'
 
-/** @public */
-export type ControlBarControls = {
-  microphone?: boolean
-  camera?: boolean
-  chat?: boolean
-  screenShare?: boolean
-  leave?: boolean
-  settings?: boolean
-}
-
-/** @public */
 export interface ControlBarProps extends React.HTMLAttributes<HTMLDivElement> {
   onDeviceError?: (error: { source: Track.Source; error: Error }) => void
-  variation?: 'minimal' | 'verbose' | 'textOnly'
-  controls?: ControlBarControls
-  /**
-   * If `true`, the user's device choices will be persisted.
-   * This will enable the user to have the same device choices when they rejoin the room.
-   * @defaultValue true
-   * @alpha
-   */
-  saveUserChoices?: boolean
 }
 
 /**
  * The `ControlBar` prefab gives the user the basic user interface to control their
  * media devices (camera, microphone and screen share), open the `Chat` and leave the room.
- *
- * @remarks
- * This component is build with other LiveKit components like `TrackToggle`,
- * `DeviceSelectorButton`, `DisconnectButton` and `StartAudio`.
- *
- * @example
- * ```tsx
- * <LiveKitRoom>
- *   <ControlBar />
- * </LiveKitRoom>
- * ```
- * @public
  */
 export function ControlBar({ onDeviceError }: ControlBarProps) {
   const isMobile = useIsMobile()
