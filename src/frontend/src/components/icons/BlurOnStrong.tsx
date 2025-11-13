e18204760b47207ec86ea456fd2028e9ac6eef8e
lebaudantoine <lebaud.antoine131@gmail.com>
2025-11-13 21:59:51 +0100

    ♿️(frontend) enhance vocalization of blur options
    Hide non-essential icons and refine the labels to emphasize that one option
    applies a stronger blur than the other. This should provide clearer cues for
    screen reader users.

--- src/frontend/src/components/icons/BlurOnStrong.tsx
+++ src/frontend/src/components/icons/BlurOnStrong.tsx
@@ -6,6 +6,7 @@ export const BlurOnStrong = () => {
       viewBox="0 0 24 24"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
+      aria-hidden="true"
     >
       <path
         fillRule="evenodd"
