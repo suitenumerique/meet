e1c6fb10f31f4f6b894dce92f37e37acf9b0f0c3
Cyril <c.gromoff@gmail.com>
2026-01-23 11:06:40 +0100

    fixup! 🐛(fix) add react-aria overlays type shim

--- src/frontend/@types/react-aria-overlays.d.ts
+++ src/frontend/@types/react-aria-overlays.d.ts
@@ -1,5 +1,18 @@
+import type * as React from 'react';
+
 declare module '@react-aria/overlays' {
-  export function useUNSAFE_PortalContext(): PortalProviderContextValue
-  export const UNSAFE_PortalProvider: (props: PortalProviderProps) => JSX.Element
+  export type PortalProviderContextValue = {
+    getContainer: () => HTMLElement | null;
+  };
+
+  export type PortalProviderProps = {
+    getContainer: () => HTMLElement | null;
+    children: React.ReactNode;
+  };
+
+  export function useUNSAFE_PortalContext(): PortalProviderContextValue;
+  export function UNSAFE_PortalProvider(
+    props: PortalProviderProps,
+  ): JSX.Element;
 }
 
