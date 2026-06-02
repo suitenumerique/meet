28f652e035bfe1f08cc606b786e9837862bbdec0
lebaudantoine <lebaud.antoine131@gmail.com>
2026-06-02 10:34:31 +0200

    🔧(backend) backport logging configuration from docs
    Backport the logging configuration from docs to LaSuite Meet after
    discussion with @lunika.
    
    Add a proper base logging setup to restore usable logs in production.
    The initial repository bootstrap lacked a complete logging
    configuration, resulting in limited operational visibility.

--- src/backend/meet/settings.py
+++ src/backend/meet/settings.py
@@ -1011,6 +1011,44 @@ class Base(Configuration):
         environ_prefix=None,
     )
 
+    # Logging
+    # We want to make it easy to log to console but by default we log production
+    # to Sentry and don't want to log to console.
+    LOGGING = {
+        "version": 1,
+        "disable_existing_loggers": False,
+        "formatters": {
+            "simple": {
+                "format": "{asctime} {name} {levelname} {message}",
+                "style": "{",
+            },
+        },
+        "handlers": {
+            "console": {
+                "class": "logging.StreamHandler",
+                "formatter": "simple",
+            },
+        },
+        # Override root logger to send it to console
+        "root": {
+            "handlers": ["console"],
+            "level": values.Value(
+                "INFO", environ_name="LOGGING_LEVEL_LOGGERS_ROOT", environ_prefix=None
+            ),
+        },
+        "loggers": {
+            "core": {
+                "handlers": ["console"],
+                "level": values.Value(
+                    "INFO",
+                    environ_name="LOGGING_LEVEL_LOGGERS_APP",
+                    environ_prefix=None,
+                ),
+                "propagate": False,
+            },
+        },
+    }
+
     # pylint: disable=invalid-name
     @property
     def ENVIRONMENT(self):
