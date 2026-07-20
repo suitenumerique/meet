1443ef12eb39846ad6462c6b93113303908bd389
Cyril <c.gromoff@gmail.com>
2026-07-20 11:21:40 +0200

    ♻️(frontend) extract getTrackKey to shared layout utils
    Move track key helper out of pip so layouts can reuse it.

--- src/frontend/src/features/pip/components/layout/PipFocusLayout.tsx
+++ src/frontend/src/features/pip/components/layout/PipFocusLayout.tsx
@@ -2,7 +2,7 @@ import { memo } from 'react'
 import type { TrackReferenceOrPlaceholder } from '@livekit/components-core'
 import { styled } from '@/styled-system/jsx'
 import { ParticipantTile } from '@/features/participantTile/components/ParticipantTile'
-import { getTrackKey } from '../../utils/pipTrackSelection'
+import { getTrackKey } from '@/features/layout/utils/trackSelection'
 
 type PipFocusLayoutProps = {
   mainTrack?: TrackReferenceOrPlaceholder
